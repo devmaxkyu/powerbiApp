d86b99cff83f66d673b192fe87d0438b7cbabc13
maxkyu <win6win@outlook.com>
2019-11-22 19:10:13 -0800

    Initial Commit

---
+++ GraphHelper.php
@@ -0,0 +1,123 @@
+<?php
+
+/**
+ * A helper class used to make calls to Microsoft Graph API.
+ */
+class AADSSO_GraphHelper
+{
+	/**
+	 * @var \AADSSO_Settings The instance of AADSSO_Settings to use.
+	 */
+	public static $settings;
+
+	/**
+	 * Gets the the Microsoft Graph API base URL to use.
+	 *
+	 * @return string The base URL to the Microsoft Graph API.
+	 */
+	public static function get_base_url() {
+		return self::$settings->graph_endpoint . '/' . self::$settings->graph_version;
+	}
+
+	/**
+	 * Checks which of the given groups the given user is a member of.
+	 *
+	 * @return mixed The response to the checkMemberGroups request.
+	 */
+	public static function user_check_member_groups( $user_id, $group_ids ) {
+		$url = self::get_base_url() . '/users/' . $user_id . '/checkMemberGroups';
+		return self::post_request( $url, array(), array( 'groupIds' => $group_ids ) );
+	}
+
+	/**
+	 * Gets the requested user.
+	 *
+	 * @return mixed The response to the user request.
+	 */
+	public static function get_user( $user_id ) {
+		$url = self::get_base_url() . '/users/' . $user_id;
+		return self::get_request( $url );
+	}
+
+	/**
+	 * Issues a GET request to the Microsoft Graph API.
+	 *
+	 * @return mixed The decoded response.
+	 */
+	public static function get_request( $url, $query_params = array() ) {
+
+		// Build the full query URL, adding api-version if necessary
+		$query_params = http_build_query( $query_params );
+		$url = $url . '?' . $query_params;
+
+		$_SESSION['aadsso_last_request'] = array(
+			'method' => 'GET',
+			'url' => $url,
+		);
+
+		AADSSO::debug_log( 'GET ' . $url, 50 );
+
+		// Make the GET request
+		$response = wp_remote_get( $url, array(
+			'headers' => self::get_required_headers_and_settings(),
+		) );
+
+		return self::parse_and_log_response( $response );
+	}
+
+	/**
+	 * Issues a POST request to the Microsoft Graph API.
+	 *
+	 * @return mixed The decoded response.
+	 */
+	public static function post_request( $url, $query_params = array(), $data = array() ) {
+
+		// Build the full query URL and encode the payload
+		$query_params = http_build_query( $query_params );
+		$url = $url . '?' . $query_params;
+		$payload = json_encode( $data );
+
+		AADSSO::debug_log( 'POST ' . $url, 50 );
+		AADSSO::debug_log( $payload, 99 );
+
+		// Make the POST request
+		$response = wp_remote_post( $url, array(
+			'body' => $payload,
+			'headers' => self::get_required_headers_and_settings(),
+		) );
+
+		return self::parse_and_log_response( $response );
+	}
+
+	/**
+	 * Logs the HTTP response headers and body and returns the JSON-decoded body.
+	 *
+	 * @return mixed The decoded response.
+	 */
+	private static function parse_and_log_response( $response ) {
+
+		$response_headers = wp_remote_retrieve_headers( $response );
+		$response_body = wp_remote_retrieve_body( $response );
+
+		AADSSO::debug_log( 'Response headers: ' . json_encode( $response_headers ), 99 );
+		AADSSO::debug_log( 'Response body: ' . json_encode( $response_body ), 50 );
+
+		return json_decode( $response_body );
+	}
+
+	/**
+	  * Returns an array with the required headers like authorization header, service version etc.
+	  *
+	  * @return array An associative array with the HTTP headers for Microsoft Graph API calls.
+	  */
+	private static function get_required_headers_and_settings()
+	{
+		// Generate the authentication header
+		return array(
+			'Authorization' => $_SESSION['aadsso_token_type'] . ' ' . $_SESSION['aadsso_access_token'],
+			'Accept'        => 'application/json',
+			'Content-Type'  => 'application/json',
+			'Prefer'        => 'return-content',
+		);
+	}
+}
---
+++ lib/powerbi_api/readme.md
@@ -0,0 +1,22 @@
+# PHP Script for Power BI REST API
+A php app to make automation for power bi service
+
+## Getting Started
+Simply clone this repository to your local machine using 'git clone'
+
+### Installing PHP libraries
+
+First install php libraries from root directory of your project using composer
+
+```
+composer install
+```
+
+## Running the tests
+
+No tests
+
+## Built With
+
+* [Power BI REST API](https://docs.microsoft.com/en-us/rest/api/power-bi/) - API docs
+* [oauth2-client](https://github.com/thephpleague/oauth2-client) - OAuth 2.0 service providers 
